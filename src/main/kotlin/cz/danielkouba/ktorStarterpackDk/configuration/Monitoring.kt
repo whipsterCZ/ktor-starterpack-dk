9e653c4eaa95c66f0a36490ff280919dae6b9ff1
Daniel Kouba <whipstercz@gmail.com>
2023-01-16 19:57:52 +0100

    Monitoring

--- src/main/kotlin/cz/danielkouba/ktorStarterpackDk/configuration/Monitoring.kt
+++ src/main/kotlin/cz/danielkouba/ktorStarterpackDk/configuration/Monitoring.kt
@@ -2,11 +2,15 @@ package cz.danielkouba.ktorStarterpackDk.configuration
 
 import io.ktor.server.application.*
 import io.ktor.server.metrics.micrometer.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.micrometer.core.instrument.Gauge
+import io.micrometer.core.instrument.config.MeterFilter
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig
 import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
+import java.time.Duration
 import java.util.concurrent.atomic.AtomicInteger
 
 fun Application.configureMonitoring() {
@@ -20,9 +24,30 @@ fun Application.configureMonitoring() {
         .builder("up") { AtomicInteger(1) }
         .description("Service is up")
 
-
     install(MicrometerMetrics) {
         registry = appMicrometerRegistry
+        metricName = this@configureMonitoring.config().metricsPrefix
+
+        timers { call, throwable ->
+            val path = call.request.path()
+            val version = parseVersion(path)
+            tag("version", version)
+        }
+
+        registry.config().meterFilter(
+            MeterFilter.renameTag("", "route", "path")
+        )
+
+        distributionStatisticConfig = DistributionStatisticConfig.Builder()
+            .percentilesHistogram(true)
+            .maximumExpectedValue(Duration.ofSeconds(5).toNanos().toDouble())
+            .minimumExpectedValue(Duration.ofMillis(20).toNanos().toDouble())
+            .serviceLevelObjectives(
+                Duration.ofMillis(100).toNanos().toDouble(),
+                Duration.ofMillis(500).toNanos().toDouble()
+            )
+            .build()
+
 
         availableGauge.register(registry)
         upGauge.register(registry)
@@ -36,5 +61,14 @@ fun Application.configureMonitoring() {
         get("/metrics") {
             call.respond(appMicrometerRegistry.scrape())
         }
+    }
+}
+
+fun parseVersion(path: String, default: String = "v1"): String {
+    val versionString: String = (path.split("/").filter { it.isNotEmpty() }.getOrNull(0) ?: "").lowercase()
+    return if ((versionString.getOrNull(0) ?: "").toString() == "v") {
+        versionString
+    } else {
+        default
     }
 }
